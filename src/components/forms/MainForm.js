958c7bccde8432dd729e58bfd202a4ae42627390
nnicusor <nicu.scurtu10@gmail.com>
2021-07-17 17:54:54 +0300

    update

--- src/components/forms/MainForm.js
+++ src/components/forms/MainForm.js
@@ -11,14 +11,44 @@ const MainForm = () => {
   const[fileExension, setFileExension]= useState('')
   const [anchor, setAnchor] = useState()
 
+    // Data
+    let fileData = null;
+    let certificateData = null;
+
   const getFile = async () => {
-    try {
+      try {
+        // Sa ma pis pe ea indexare si pe el program
       const data = await fetch('http://localhost:3000/upload', {
         method: 'GET',
-      })
-      const arrayBuffer = await data.arrayBuffer()
-      const base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(arrayBuffer)));
-      const dowload = React.createElement('a', {href: 'data:application/octet-stream;base64,' + base64String, download: 'text.png'}, "Download" )
+      }).then(res => res.text())
+          .then(result => {
+              return result;
+          })
+            .catch((err) => console.log(err))
+          /*
+          const arrayBuffer = await data;
+
+          console.log("data", data);
+          return;
+
+        //ceva
+        var uintArray = new Uint8Array(arrayBuffer);
+        var converted = [];
+          uintArray.forEach(function (byte) { converted.push(String.fromCharCode(byte)) });
+          console.log("uintArray,", uintArray);
+
+        var byteArray = "";
+        for (var i = 0; i < converted.length; i++) {
+            byteArray += converted[i];
+        }
+          //converted = byteArray;
+
+          console.log("Convertit", converted);
+
+          const base64String = btoa(...converted);
+          //console.log("Base64", base64String);
+          */
+      const dowload = React.createElement('a', {href: 'data:application/octet-stream;base64,' + data, download: `${fileData.fileName}${fileData.fileExtension}`}, "Download" )
       setAnchor(dowload)
       console.log(dowload);
     } catch (e) {
@@ -27,14 +57,13 @@ const MainForm = () => {
   }
 
   const onSubmit = async (data) => {
-    const { upload_file, upload_key, upload_signature, aes_type, encrypt_action, encrypt_type, signature_type, keyPassword,iv } = data
+      const { upload_file, upload_key, upload_signature, aes_type, encrypt_action, encrypt_type, signature_type, keyPassword, iv } = data
+      await getCertificate();
+
 
     // aesecb.en
     const encryptType = encrypt_type === 'aes' ? aes_type : 'rsa'
     const encryptionAction = `${encryptType}.${encrypt_action}`
-
-   console.log(data);
-
 
     // await fetch('http://localhost:3000/users', {
     //   method: 'POST',
@@ -44,24 +73,47 @@ const MainForm = () => {
     //   .then((res) => res)
     //   .then((result) => console.log(result))
     //   .catch((err) => console.log(err))
+
     const jsonData = {
       ...jsonBody,
       services: [encryptionAction, signature_type],
       aesData: {
         keyPassword,
         iv
-      }
+        },
+        keysData: {
+            certificate: certificateData.certificate,
+        }
     }
 
-    await request('users', 'POST',  { "Content-Type": "Application/json" }, JSON.stringify(jsonData))
+      await request('users', 'POST', { "Content-Type": "Application/json" }, JSON.stringify(jsonData))
+
     const formData = new FormData()
     formData.append('fileData', upload_file[0])
     formData.append('signatureData', upload_signature[0])
     formData.append('keysData', upload_key[0])
-    const uploadPostResponse = request('upload', 'POST', {}, formData)
-    console.log(await uploadPostResponse);
+
+      fileData = await request('upload', 'POST', {}, formData)
+
+      console.log("fileExtension,", fileData);
+
     await getFile();
-  }
+    }
+
+    const getCertificate = async () => {
+        certificateData = await fetch('http://localhost:8888/', {
+            method: 'GET',
+        }).then(res => res.json())
+            .then(result => {
+                console.log("Certificate:", b64_to_utf8(result.certificate))
+                return result;
+            })
+            .catch((err) => console.log(err))
+    }
+
+    function b64_to_utf8(str) {
+        return decodeURIComponent(escape(window.atob(str)));
+    }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="container mx-auto pt-12">
--- src/general.js
+++ src/general.js
@@ -26,12 +26,16 @@ export const jsonBody = {
 
 
 export const request = async (endpoint, method, headers = { "Content-Type": "Application/json" }, body = JSON.stringify(jsonBody)) => {
-  await fetch(`http://localhost:3000/${endpoint}`, {
+  const response = await fetch(`http://localhost:3000/${endpoint}`, {
     method,
     headers,
     body
   })
-    .then(res => res)
-    .then(result => result.json())
-    .catch((err) => console.log(err))
+    .then(res => res.json())
+      .then(result => {
+          console.log("Result:", result)
+          return result;
+      })
+        .catch((err) => console.log(err))
+    return response
 }
